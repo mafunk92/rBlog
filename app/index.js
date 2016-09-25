5318edb00b15ba0ae4e093a3e55aef22452bf93a
funky <mafunk92@gmail.com>
2016-09-24 23:24:32 -0500

    change from browserHistory to hashHistory

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
@@ -13,6 +13,6 @@ const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-        <Router history={ browserHistory } routes={ routes } />
+        <Router history={ hashHistory } routes={ routes } />
     </Provider>
     , document.getElementById('app'));
